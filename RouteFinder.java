ae1a2a78b1112595277429c29f0d1a9076a1ad98
sandipkarande <sandip.karande@gmail.com>
2015-06-30 07:36:06 +0200

    Create RouteFinder.java

---
+++ RouteFinder.java
@@ -0,0 +1,35 @@
+public void computePaths(Vertex source)
+    {
+        source.minDistance = 0.;
+        PriorityQueue<Vertex> vertexQueue = new PriorityQueue<Vertex>();
+        vertexQueue.add(source);
+        while (!vertexQueue.isEmpty()) {
+            Vertex u = vertexQueue.poll();
+            // Visit each edge exiting u
+            for (Edge e : u.adjacencies)
+            {
+                Vertex v = e.target;
+                double weight = e.weight;
+                double distanceThroughU = u.minDistance + weight;
+                if (distanceThroughU < v.minDistance) {
+                    vertexQueue.remove(v);
+                    v.minDistance = distanceThroughU ;
+                    v.previous = u;
+                    vertexQueue.add(v);
+                }
+            }
+        }
+    }
+    /** Used to return the shortest path from a source to destination station.
+     * @param target destination station
+     * @return shortest path from source station to destination station.
+     */
+    public static List<Vertex> getShortestPathTo(Vertex target)
+    {
+        List<Vertex> path = new ArrayList<Vertex>();
+        for (Vertex vertex = target; vertex != null; vertex = vertex.previous)
+            path.add(vertex);
+        Collections.reverse(path);
+        return path;
+    }
+}
